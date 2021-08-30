6268524507e7115915c376ebd628655ab3012d5e
Your Name <you@example.com>
2021-08-30 15:22:22 +0900

    articles.html step1

--- app.py
+++ app.py
@@ -1,18 +1,50 @@
-from flask import Flask 
+from flask import Flask , render_template
+from data import Articles # data파일에서 함수이름 가져온거임 
 # flask 가져오기 
 
 app = Flask(__name__)
 # 서버 띄우기 
 # __name__:자체내장변수 
 
-@app.route('/')
+@app.route('/hello')
 def hello_world():
     return 'Hello World!'
 # 반응하는 코드를 만드는 애 
 # / : 경로 /뒤에 hello를 쓰면 접속주소가 http://localhost:5000/ 에서 -->http://localhost:5000/hello 이걸로 바뀜 
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
 # 서버 띄우기 
 # 왜 굳이 이걸 쓰냐?  플라스크를 실행시켰을 때 자체내장변수를 사용하여 if문을 만들고 main일 때만 앱을 실행시켜라 하는거고 python이 아닌걸로
 # 실행시엔 실행을 못하게 하려고 이렇게 만든거다 
+
+# 게시판 만들기 // flask 라이브러리를 이용한 웹 구현 
+# front end - 보여지는 부분 
+# back end - 기능을 하는 부분 (서버) -> back end 안에 database가 있음 // back end 안에 front end가 있는 것도 있고 외부에 있는 것도 있음 
+# front end가 back end에 requst를 날리면 response를 해주는 방식 
+# 우리가 하는 방식은 back end안에 database 와 front end가 같이 있는 방식으로 만들 것 
+# flask 파이썬 라이브러리 및 프레임워크 를 사용하여 만들거야
+# flask_web 안에 templates, static, app.py 의 폴더2개와 파일1개를 만들고 시작할 거임
+# templates --> front end가 들어갈 거임 
+# static 
+# app.py -> 서버 즉 back end가 들어갈 거임 
+
+# http://localhost:5000 경로로 get 방식으로 request 할때 index.html 파일을 랜더링 해주는 기능을 구현하기위하여 
+# app.py에 다음과 같은 코드를 추가한다. 
+# @ : 데코레이터, 처음꺼(함수나 메서드) 처리해주고나서 @에 붙어있는 애를 처리해줌
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = 'KIM'
+    return render_template('index.html', data=name) # render_template 플라스크안에있는 라이브러리이기 때문에 위에서 import 해줘야함 
+    # render_template 를 실행시키면 templates 라는 폴더를 찾게됨. 그안에있는 index.html파일의 내용이 localhost:5000에 표기됨
+    # render_template는 index.html를 랜더링 해주면서 data를 실어보낼수 있음 
+
+@app.route('/articles', methods=['GET','POST'])
+def articles():
+    list_data = Articles()
+    return render_template('articles.html', data=list_data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
